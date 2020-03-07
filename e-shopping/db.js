1894560d3b10d9e9eb728fffef3300d5ec238934
“bikale” <bikaleg@gmail.com>
2020-03-07 00:58:23 -0600

    applying mongoDB regex and query operators

--- e-shopping/db.js
+++ e-shopping/db.js
@@ -1,18 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-
+const objectId = mongodb.ObjectID;
 MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
   .then(clinet => {
     console.log('db connected.......');
     clinet
-      .db('miu')
-      .collection('student')
-      .find()
+      .db('labDb')
+      .collection('restaurants')
+      .aggregate([
+        {
+          $match: {
+            cuisine: 'Ethiopian'
+          }
+        },
+        { $unwind: '$grades' },
+        {
+          $group: {
+            _id: '$restaurant_id',
+            scores: { $avg: '$grades.score' },
+            name:{$addToSet:'$name'}
+          }
+        }
+      ])
       .toArray()
-      
-      
-      .then(data=>{console.log(data);})
-    // insertMany([{'name':'Getch','course':['CS456','CS452']},{'name':'Tk','course':['CS456','CS452']}])
+      .then(console.log);
   })
   .catch(err => {
     console.log('not connected');
